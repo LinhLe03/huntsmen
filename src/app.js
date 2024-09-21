ba923444419bbdc3c24c1e280bb5911919e46833
Brody <brodydwatson@gmail.com>
2024-09-21 20:57:22 +1000

    post v0.0001

--- src/app.js
+++ src/app.js
@@ -2,26 +2,19 @@ window.onload = (event) =>{
     const img = document.getElementById("bee-icon");
     var imgNum = Math.floor(Math.random() * 4);
     img.src = "images/0" + imgNum.toString() + ".png";   
-    
-    // load posts
-    loadJson()
-
-    // const header = document.getElementById("post")
-    
-    // const post = postdata.posts;
-    // for(const post of posts) {
-    //     const postTitle = document.createElement("h2");
-        
-    //     postTitle.textContent = post.Title;
-    //     header.append(postTitle)
-    // }
 };
 
-async function loadJson(){
-    const response = await fetch('data.json')
-    const json = await response.json()
+// view live update for inputs into post creation
+function updateInput(i, output) {
+    document.getElementById(output).innerText = i.value;
+} 
 
+function post() {
+    const role = document.getElementById("role-output").innerText
+    const project = document.getElementById("project-output").innerText
+    const body = document.getElementById("body-output").innerText
 
-    console.log(json)
-}   
-
+    console.log("this would post a post with the title: " + role)
+    console.log("Alongside the project name: " + project)
+    console.log("And with some information about the role: " + body)  
+}

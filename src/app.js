9a1ce25f2b288a140d651381944bb3051b2b930f
zafir <zafirhasan2006@gmail.com>
2024-09-21 22:02:28 +1000

    Add js code to change active post

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ window.onload = (event) =>{
     img.src = "images/0" + imgNum.toString() + ".png";   
     
     // load posts
-    loadJson()
+    loadData();
 
     // const header = document.getElementById("post")
     
@@ -17,11 +17,70 @@ window.onload = (event) =>{
     // }
 };
 
-async function loadJson(){
-    const response = await fetch('data.json')
-    const json = await response.json()
+async function loadData(){
+    const response = await fetch('data.json');
+    const json = await response.json();
 
+    const cardsContainer = document.querySelector(".content .posts");
+    CreateCardsAndAppend(cardsContainer, json);
 
-    console.log(json)
-}   
+    const cards = document.querySelectorAll(".card");
+    cards.forEach((card) => {
+        card.addEventListener('click', () => {
+            if (card.classList.contains("selected")) {
+                card.classList.remove("selected");
+            }
+            else {
+                cards.forEach((c) => {
+                if (c.classList.contains("selected")) {
+                    c.classList.remove("selected");
+                }
+                });
+                card.classList.toggle("selected");
+                let selectedID = card.id;
+                for (let i = 0; i < json.posts.length; ++i) {
+                    if (json.posts[i].id != selectedID)
+                        continue;
+                    const activePost = document.querySelector(".active-post .text");
+                    activePost.innerHTML = "";
+                    const title = document.createElement("h1");
+                    title.textContent = json.posts[i].title;
+                    const detailsText = document.createElement("div");
+                    detailsText.textContent = json.posts[i].details;
+                    const requirements = document.createElement("ul");
+                    for (let j = 0; j < json.posts[i].requirements.length; ++j) {
+                        const requirement = document.createElement("li");
+                        requirement.textContent = json.posts[i].requirements[j];
+                        requirements.appendChild(requirement);
+                    }
+                    activePost.append(title, detailsText, requirements);
+                }
+            }
+        });
+    });
+}
 
+function CreateCardsAndAppend(cardsContainer, json) {
+    for (let i = 0; i < json.posts.length; ++i) {
+        const card = document.createElement("section");
+        card.id = json.posts[i].id;
+        card.classList.add("card");
+        const container = document.createElement("div");
+        container.classList.toggle("container");
+        const title = document.createElement("h2");
+        title.textContent = json.posts[i].title;
+        const responsibilities = document.createElement("ul");
+        for (let j = 0; j < json.posts[i].responsibilities.length; ++j) {
+            const r = document.createElement("li");
+            r.textContent = json.posts[i].responsibilities[j];
+            responsibilities.appendChild(r);
+        }
+        const date = document.createElement("span");
+        date.textContent = "01/01/01";
+        container.appendChild(title);
+        container.appendChild(responsibilities);
+        card.appendChild(container);
+        card.appendChild(date);
+        cardsContainer.appendChild(card);
+    }
+}
--- src/home.js
+++ src/home.js
@@ -1,17 +1,1 @@
-const cards = document.querySelectorAll(".card");
 
-cards.forEach((card) => {
-    card.addEventListener('click', () => {
-        if (card.classList.contains("selected")) {
-            card.classList.remove("selected");
-        }
-        else {
-            cards.forEach((c) => {
-            if (c.classList.contains("selected")) {
-                c.classList.remove("selected")
-            }
-            });
-            card.classList.toggle("selected");
-        }
-    });
-});
